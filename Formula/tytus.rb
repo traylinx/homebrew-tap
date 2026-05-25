c408faf5dc56d56ed2bdc8c57f5d92e52f09be44
Harvey <harvey@makakoo.local>
2026-05-25 20:00:46 +0200

    tytus 0.7.9

--- Formula/tytus.rb
+++ Formula/tytus.rb
@@ -17,24 +17,24 @@
 class Tytus < Formula
   desc "Private AI pod CLI — connect any terminal to your isolated LLM gateway"
   homepage "https://get.traylinx.com"
-  version "0.7.3"
+  version "0.7.9"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/traylinx/tytus-cli/releases/download/v#{version}/tytus-macos-aarch64.tar.gz"
-      sha256 "033f1b263361ddcc83b4095264b9e98f952ca116658c53d72261da48f9d0e79c"
+      sha256 "9297ab7c963e46066509f16772b905b1be5233eec35ec5b6d7047281c7fbf2ce"
     end
     on_intel do
       url "https://github.com/traylinx/tytus-cli/releases/download/v#{version}/tytus-macos-x86_64.tar.gz"
-      sha256 "d634675383405a5e6bee705fb7111b6b63781cc6dc65654bf4d1e8ba9a22adc7"
+      sha256 "9581b87af845e80011bc44a20ef946f3f739b36f2ffd2fe0ad1a5602cffd9d42"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/traylinx/tytus-cli/releases/download/v#{version}/tytus-linux-x86_64.tar.gz"
-      sha256 "292631a26924beac38ce1e4f2c1af99226cfc4f6d5cd393ba11b019d9c4f6853"
+      sha256 "3fa19646bf5564cbecb6f48ad30de9c261b745a8a6bc5970355d7ef6c824dcfe"
     end
   end
 
